a65fb55e98747c9810e309f04c12e56c00c4f5ef
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-18 18:02:03 +0300

    Update capture.py

--- src/capture.py
+++ src/capture.py
@@ -1,14 +1,13 @@
-import requests
 from random import randint
-
+from requests import Session
 
 class Capture:
-	def __init__(
-			self,
-			session_id: int = str(randint(1, 9999))) -> None:
+	def __init__(self, session_id: str = None)) -> None:
 		self.api = "https://capture.chat/api"
-		self.headers = {
-			"user-agent": "15 Android/56 ru-RU",
+		session_id = session_id or str(randint(1, 9999))
+		self.session = Session()
+		self.session.headers = {
+			"User-Agent": "15 Android/56 ru-RU",
 			"session-id": session_id
 		}
 		self.x_token = None
@@ -18,14 +17,12 @@ class Capture:
 		data = {
 			"id_token": google_id_token
 		}
-		return requests.post(
-			f"{self.api}/auth/google",
-			data=data,
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/auth/google", data=data).json()
 
 	def login_with_x_token(self, x_token: str) -> dict:
 		self.x_token = x_token
-		self.headers["X-Token"] = self.x_token
+		self.session.headers["X-Token"] = self.x_token
 		response = self.get_account_info()
 		if "user_id" in response["user"]:
 			self.user_id = response["user"]["user_id"]
@@ -35,10 +32,8 @@ class Capture:
 		data = {
 			"username": username
 		}
-		return requests.post(
-			f"{self.api}/auth/username",
-			data=data,
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/auth/username", data=data).json()
 
 	def register_with_google(
 			self,
@@ -48,106 +43,81 @@ class Capture:
 			"id_token": google_id_token,
 			"username": username
 		}
-		return requests.post(
-			f"{self.api}/auth/google/signup",
-			data=data,
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/auth/google/signup", data=data).json()
 
 	def get_notifications(self) -> dict:
-		return requests.get(
-			f"{self.api}/notification",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/notification").json()
 
 	def get_chat_users(
 			self,
 			chat_id: int,
 			count: int = 10) -> dict:
-		return requests.get(
-			f"{self.api}/topic/{chat_id}/member?count={count}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/topic/{chat_id}/member?count={count}").json()
 
 	def get_user_chats(
 			self,
 			user_id: str,
 			count: int = 10) -> dict:
-		return requests.get(
-			f"{self.api}/user/{user_id}/topic?count={count}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/user/{user_id}/topic?count={count}").json()
 
 	def get_discover_items(self, count: int = 10) -> dict:
-		return requests.get(
-			f"{self.api}/topic/discover?count={count}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/topic/discover?count={count}").json()
 
 	def get_discover_onboarding(self) -> dict:
-		return requests.get(
-			f"{self.api}/discover/onboarding",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/discover/onboarding").json()
 
 	def get_discover_communities(self) -> dict:
-		return requests.get(
-			f"{self.api}/community/discover",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/community/discover").json()
 
 	def get_chats(self, count: int = 10) -> dict:
-		return requests.get(
-			f"{self.api}/topic?count={count}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/topic?count={count}").json()
 
 	def get_direct_messages_requests(self) -> dict:
-		return requests.get(
-			f"{self.api}/dm/request",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/dm/request").json()
 
 	def get_all_chats(self) -> dict:
-		return requests.get(
-			f"{self.api}/topic/all",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/topic/all").json()
 
 	def get_chat(self, chat_id: int) -> dict:
-		return requests.get(
-			f"{self.api}/topic/{chat_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/topic/{chat_id}").json()
 
 	def get_chat_by_link(self, link: str) -> dict:
-		return requests.get(
-			f"{self.api}/link/{link}",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/link/{link}").json()
 
 	def get_user(self, user_id: str) -> dict:
-		return requests.get(
-			f"{self.api}/user/{user_id}",
-			headers=self.headers).json()
-
-	def get_trending_gifs(self) -> dict:
-		return requests.get(
-			f"{self.api}/gifs/trending",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/user/{user_id}").json()
 
 	def get_chat_photos(
 			self,
 			chat_id: int,
 			count: str = 50,
 			reverse: bool = True) -> dict:
-		return requests.get(
-			f"{self.api}/topic/{chat_id}/photo?count={count}&reverse={reverse}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/topic/{chat_id}/photo?count={count}&reverse={reverse}").json()
 
 	def get_chat_messages(
 			self,
 			chat_id: int,
 			count: int = 10) -> dict:
-		return requests.get(
-			f"{self.api}/topic/{chat_id}/message?count={count}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/topic/{chat_id}/message?count={count}").json()
 
 	def search_mention_users(
 			self,
 			chat_id: int,
 			prefix: str = "") -> dict:
-		return requests.get(
-			f"{self.api}/topic/{chat_id}/mention/suggest?prefix={prefix}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/topic/{chat_id}/mention/suggest?prefix={prefix}").json()
 
 	def send_message(
 			self,
@@ -165,10 +135,8 @@ class Capture:
 			data["gif_id"] = gif_id
 		if gif_provider:
 			data["gif_provider"] = gif_provider
-		return requests.post(
-			f"{self.api}/topic/{chat_id}/message",
-			data=data,
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/topic/{chat_id}/message", data=data).json()
 
 	def create_chat(
 			self,
@@ -182,10 +150,8 @@ class Capture:
 			"name": name,
 			"content": content
 		}
-		return requests.post(
-			f"{self.api}/topic",
-			data=data,
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/topic", data=data).json()
 
 	def edit_chat(
 			self,
@@ -198,28 +164,23 @@ class Capture:
 			"name": name,
 			"discription": description
 		}
-		return requests.patch(
-			f"{self.api}/topicId/{chat_id}",
-			data=data,
-			headers=self.headers).json()
+		return self.session.patch(
+			f"{self.api}/topicId/{chat_id}", data=data).json()
 
 	def delete_message(
 			self,
 			chat_id: int,
 			message_id: str) -> dict:
-		return requests.delete(
-			f"{self.api}/topic/{chat_id}/message{message_id}",
-			headers=self.headers).json()
+		return self.session.delete(
+			f"{self.api}/topic/{chat_id}/message{message_id}").json()
 
 	def join_chat(self, chat_id: int) -> dict:
-		return requests.post(
-			f"{self.api}/topic/{chat_id}/subscription",
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/topic/{chat_id}/subscription").json()
 
 	def leave_chat(self, chat_id: int) -> dict:
-		return requests.delete(
-			f"{self.api}/topic/{chat_id}/subscription",
-			headers=self.headers).json()
+		return self.session.delete(
+			f"{self.api}/topic/{chat_id}/subscription").json()
 
 	def edit_profile(
 			self,
@@ -236,10 +197,8 @@ class Capture:
 			data["bio"] = bio
 		if location:
 			data["location"] = location
-		return requests.patch(
-			f"{self.api}/user/me",
-			data=data,
-			headers=self.headers).json()
+		return self.session.patch(
+			f"{self.api}/user/me", data=data).json()
 
 	def set_chat_user_role(
 			self,
@@ -249,10 +208,8 @@ class Capture:
 		data = {
 			"role": role
 		}
-		return requests.patch(
-			f"{self.api}/topic/{chat_id}/member/{user_id}",
-			data=data,
-			headers=self.headers).json()
+		return self.session.patch(
+			f"{self.api}/topic/{chat_id}/member/{user_id}", data=data).json()
 
 	def edit_message(
 			self,
@@ -262,15 +219,12 @@ class Capture:
 		data = {
 			"text": text
 		}
-		return requests.patch(
-			f"{self.api}/topic/{chat_id}/message/{message_id}",
-			data=data,
-			headers=self.headers).json()
+		return self.session.patch(
+			f"{self.api}/topic/{chat_id}/message/{message_id}", data=data).json()
 
 	def ban(self, chat_id: int, user_id: str) -> dict:
-		return requests.post(
-			f"{self.api}/topic/{chat_id}/ban/{user_id}",
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/topic/{chat_id}/ban/{user_id}").json()
 
 	def report_user(
 			self, 
@@ -282,10 +236,8 @@ class Capture:
 		}
 		if comment:
 			data["comment"] = comment
-		return requests.post(
-			f"{self.api}/user/{user_id}/report",
-			data=data,
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/user/{user_id}/report", data=data).json()
 
 	def report_message(
 			self,
@@ -298,10 +250,8 @@ class Capture:
 		}
 		if comment:
 			data["comment"] = comment
-		return requests.post(
-			f"{self.api}/topic/{chat_id}/message/{message_id}",
-			data=data,
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/topic/{chat_id}/message/{message_id}", data=data).json()
 
 	def report_chat(
 			self,
@@ -313,91 +263,70 @@ class Capture:
 		}
 		if comment:
 			data["comment"] = comment
-		return requests.post(
-			f"{self.api}/topic/{chat_id}/report",
-			data=data,
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/topic/{chat_id}/report", data=data).json()
 
 	def unban(self, chat_id: int, user_id: str) -> dict:
-		return requests.delete(
-			f"{self.api}/topic/{chat_id}/ban/{user_id}",
-			headers=self.headers).json()
+		return self.session.delete(
+			f"{self.api}/topic/{chat_id}/ban/{user_id}").json()
 
 	def send_direct_message_request(self, user_id: str) -> dict:
-		return requests.post(
-			f"{self.api}/dm/user/{user_id}",
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/dm/user/{user_id}").json()
 
 	def follow_chat(self, chat_id: int) -> dict:
-		return requests.post(
-			f"{self.api}/topic/{chat_id}/follow",
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/topic/{chat_id}/follow").json()
 
 	def unfollow_chat(self, chat_id: int) -> dict:
-		return requests.delete(
-			f"{self.api}/topic/{chat_id}/unfollow",
-			headers=self.headers).json()
+		return self.session.delete(
+			f"{self.api}/topic/{chat_id}/unfollow").json()
 
 	def search_chats(self, query: str) -> dict:
-		return requests.get(
-			f"{self.api}/topic/search/{query}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/topic/search/{query}").json()
 
 	def search_gifs(self, query: str) -> dict:
-		return requests.get(
-			f"{self.api}/gifs/search/{query}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/gifs/search/{query}").json()
 
 	def unread(self, chat_id: int) -> dict:
-		return requests.delete(
-			f"{self.api}/topic/{chat_id}/unread",
-			headers=self.headers).json()
+		return self.session.delete(
+			f"{self.api}/topic/{chat_id}/unread").json()
 
 	def post_typing(
 			self,
 			chat_id: int,
 			status: str = "text") -> dict:
-		return requests.post(
-			f"{self.api}/topic/{chat_id}/typing?status={status}",
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/topic/{chat_id}/typing?status={status}").json()
 
 	def get_config(self) -> dict:
-		return requests.get(
-			f"{self.api}/config", headers=self.headers).json()
+		return self.session.get(f"{self.api}/config").json()
 
 	def get_account_info(self) -> dict:
-		return requests.get(
-			f"{self.api}/user/me", headers=self.headers).json()
+		return self.session.get(f"{self.api}/user/me").json()
 
 	def get_ml_config(self) -> dict:
-		return requests.get(
-			f"{self.api}/ml/config",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/ml/config").json()
 
 	def get_joined_communities(self) -> dict:
-		return requests.get(
-			f"{self.api}/community",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/community").json()
 
 	def get_stickers(self) -> dict:
-		return requests.get(
-			f"{self.api}/stickers",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/stickers").json()
 
 	def get_trending_gifs(self) -> dict:
-		return requests.get(
-			f"{self.api}/gifs/trending",
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/gifs/trending").json()
 
 	def get_user_badges(self, user_id: str) -> dict:
-		return requests.get(
-			f"{self.api}/user/{user_id}/badges",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/user/{user_id}/badges").json()
 
 	def get_user_by_username(self, username: str) -> dict:
-		return requests.get(
-			f"{self.api}/user/screen_name/{username}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/user/screen_name/{username}").json()
 
 	def update_chat_settings(
 			self,
@@ -408,48 +337,39 @@ class Capture:
 			"sound": sound,
 			"mute": mute
 		}
-		return requests.patch(
-			f"{self.api}/topic/{chat_id}/settings",
-			data=data,
-			headers=self.headers).json()
+		return self.session.patch(
+			f"{self.api}/topic/{chat_id}/settings", data=data).json()
 
 	def get_community(self, com_id: int) -> dict:
-		return requests.get(
-			f"{self.api}/community/{com_id}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/community/{com_id}").json()
 
 	def get_community_chats(self, com_id: int) -> dict:
-		return requests.get(
-			f"{self.api}/community/{com_id}/topic",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/community/{com_id}/topic").json()
 
 	def get_link_preview(self, url: str) -> dict:
-		return requests.get(
-			f"{self.api}/link/preview?url={url}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/link/preview?url={url}").json()
 
 	def get_relevant_chat_photos(
 			self,
 			chat_id: int,
 			count: int = 10) -> dict:
-		return requests.get(
-			f"{self.api}/topic/{chat_id}/ml/photo?count={count}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/topic/{chat_id}/ml/photo?count={count}").json()
 
 	def remove_chat_from_community(
 			self,
 			com_id: int,
 			chat_id: int) -> dict:
-		return requests.delete(
-			f"{self.api}/community/{com_id}/topic/{chat_id}",
-			headers=self.headers).json()
+		return self.session.delete(
+			f"{self.api}/community/{com_id}/topic/{chat_id}").json()
 
 	def follow_community(self, com_id: int) -> dict:
-		return requests.post(
-			f"{self.api}/community/{com_id}/member",
-			headers=self.headers).json()
+		return self.session.post(
+			f"{self.api}/community/{com_id}/member").json()
 
 	def unfollow_community(self, com_id: int) -> dict:
-		return requests.delete(
-			f"{self.api}/community/{com_id}/member",
-			headers=self.headers).json()
+		return self.session.delete(
+			f"{self.api}/community/{com_id}/member").json()
